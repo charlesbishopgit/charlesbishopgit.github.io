398709f88dac085f7e8f90579152783dc30d48ca
charlesbishopgit <146463945+charlesbishopgit@users.noreply.github.com>
2023-10-03 09:29:47 -0600

    This is my website

---
+++ scripts/forestKanji.js
@@ -0,0 +1,10 @@
+// JavaScript 
+
+anime({
+	targets: '#kanji path',
+	strokeDashoffset: [anime.setDashoffset, 0],
+	easing: 'easeInOutQuad',
+	duration: '5000',
+	direction: 'alternate',
+	loop: true
+});
